30af54d8045da7225036577d1068be0fc33ec4f6
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-07-05 15:07:09 -0400

    Update etl.py

--- vertex/source_code/source_code/etl.py
+++ vertex/source_code/source_code/etl.py
@@ -8,7 +8,7 @@ import os
 from io import BytesIO
 import json
 import requests
-from typing import Tuple, Optional, Callable, Dict
+from typing import Tuple, Optional, Callable, Dict, Any
 from PIL.Image import Image, open as load_image, DecompressionBombError
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
