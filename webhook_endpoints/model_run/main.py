3c959fc2151ac750b5b0d5470ba9a3f0f7d36ae3
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-07-06 12:41:21 -0400

    Update main.py

--- webhook_endpoints/model_run/main.py
+++ webhook_endpoints/model_run/main.py
@@ -4,6 +4,8 @@ def model_run(request):
     import json
     from source_code.config import env_vars, create_gcs_key, get_lb_client, get_gcs_client
     from source_code.etl import etl_job, upload_ndjson_data
+    from labelbox import Client
+    from google.cloud import storage
     
     string = request.get_data()
     request_data = json.loads(string)
@@ -12,16 +14,13 @@ def model_run(request):
     model_run_id = request_data['modelRunId']
     model_type = request_data['modelType']
 
-    api_key = env_vars('api_key')
-    gcs_bucket = env_vars('gcs_bucket')
-
-    lb_client = get_lb_client(api_key)
-    bucket = get_gcs_client().bucket(gcs_bucket)
+    lb_client = get_lb_client()
+    bucket = get_gcs_client().create_bucket(env_vars('gcs_bucket'), location = 'US-CENTRAL1')
 
     json_data = etl_job(lb_client, model_run_id, bucket)
     gcs_key = create_gcs_key(model_run_id)
     etl_file = upload_ndjson_data(json_data, bucket, gcs_key)
     
-    logger.info("ETL Complete. URI: %s", f"{etl_file}")
+    print(f"ETL Complete. URI: %s", "{etl_file}")
 
-    return "Training Job"
+    return "ETL Job"
