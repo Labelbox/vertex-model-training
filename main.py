bfd5d44db548ddb177f605d9936728341d7f9f5b
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-07-14 19:37:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 def monitor_function(request):
+    import json
+    import time
     from google.cloud import aiplatform
     from source_code.config import env_vars 
     
-    job_name = env_vars("job_name")
+    request_bytes = request.get_data()
+    request_json = json.loads(request_bytes)
     
-    training_job = aiplatform.AutoMLImageTrainingJob.list(filter=f'display_name={job_name}')[0]
+    time.sleep(300)
     
-    try:
-        print('Use parenthesis')
-        x = str(training_job.state())
-    except:
-        print('Dont')   
-        x = str(training_job.state)
+    training_job = aiplatform.AutoMLImageTrainingJob.list(filter=f'display_name={request_json['job_name']}')[0]
+    
+    job_state = str(training_job.state)
     
     completed_states = [
         "PipelineState.PIPELINE_STATE_SUCCEEDED",
@@ -20,36 +20,37 @@ def monitor_function(request):
         "PipelineState.PIPELINE_STATE_PAUSED",     
     ]
     
-    if x in completed_states:
+    if job_state in completed_states:
         print('Training compete, send to inference')
+        # requests.post(monitor_url, data=request_bytes)
     else:
-        print('Training incomplete, will check again in X minutes')
+        print('Training incomplete, will check again in 5 minutes')
+        requests.post(request_json['monitor_url'], data=request_bytes)
     
     return "Monitor Job"
     
 
 def train_function(request):
+    """
+    Initates a training job and the monitor cloud function
+    """
     import json
+    import requests
     from source_code.config import env_vars    
     from source_code.etl import create_vertex_dataset
     from source_code.train import create_autoML_training_job
     
     request_bytes = request.get_data()
-    print(request_bytes)
-    
     request_json = json.loads(request_bytes)
-    print(request_json)
-    
-    model_run_id = list(request_json.keys())[0]
-    etl_file = list(request_json.values())[0]    
-    
-    model_name = env_vars('model_name')
 
-    vertex_dataset = create_vertex_dataset(model_run_id, etl_file)
+    vertex_dataset = create_vertex_dataset(request_json['model_run_id'], request_json['etl_file'])
+    
+    requests.post(request_json['monitor_url'], data=request_bytes)    
+    
     vertex_model, vertex_model_id = create_autoML_training_job(model_name, vertex_dataset, model_run_id)
     
+    print(f'Training Job Name: {model_name}')
     print(f'Vertex Model ID: {vertex_model_id}')
-    print(f'Vertex Model: {vertex_model}')
     
     return "Train Job"
 
@@ -60,6 +61,9 @@ def etl_function(request):
         api_key         :       Labelbox API Key
         gcs_bucket      :       Name of a GCS Bucket to-be-generated
         google_project  :       Name of the google project this Cloud Function is in
+    Returns:
+        Google Bucket with the data rows from the model run, ready to-be-converted into a Vertex Dataset
+        A dictionary where {model_run_id : etl_file}
     """
     import json
     from source_code.config import env_vars, create_gcs_key, get_lb_client, get_gcs_client
@@ -91,9 +95,20 @@ def etl_function(request):
     
     print(f'ETL File: {etl_file}')
     
-    post_bytes = json.dumps({str(model_run_id) : str(etl_file)}).encode('utf-8')
-    train_url = env_vars("train_url")
-    requests.post(train_url, data=post_bytes)
+    post_dict {
+        "model_run_id" : model_run_id,
+        "etl_file" : etl_file,
+        "api_key" : env_vars("api_key"),
+        "google_project" : env_vars("google_project"),
+        "train_url" : env_vars("train_url"),
+        "monitor_url" : env_vars("monitor_url"),
+        "inference_url" : env_vars("inference_url"),
+        "model_name" : env_vars("model_name")
+    }
+    
+    post_bytes = json.dumps(post_dict).encode('utf-8')
+    
+    requests.post(post_dict['train_url'], data=post_bytes)
     
     print(f"ETL Complete. Training Job Initiated.")
 
