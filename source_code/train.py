b866f22545ab94d26a2aea7d6619122f2dabd087
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-07-14 19:14:35 -0400

    Update train.py

--- source_code/train.py
+++ source_code/train.py
@@ -1,11 +1,11 @@
 from google.cloud import aiplatform
 
-def create_autoML_training_job(name: str, vertex_dataset, model_run_id):
+def create_autoML_training_job(model_name: str, vertex_dataset, model_run_id):
   
-  vertex_model_id = str("model-") + str(model_run_id)
+  vertex_model_id = str(model_name) + "-" + str(model_run_id)
   
   job = aiplatform.AutoMLImageTrainingJob(
-    display_name = name,
+    display_name = model_name,
     prediction_type = "classification",
     multi_label = False,
     model_type = "MOBILE_TF_VERSATILE_1"
@@ -15,7 +15,7 @@ def create_autoML_training_job(name: str, vertex_dataset, model_run_id):
     dataset = vertex_dataset,
     sync = False,
     model_id = vertex_model_id,
-    model_display_name = name,
+    model_display_name = vertex_model_id,
     budget_milli_node_hours = 20000,
     training_filter_split = "labels.aiplatform.googleapis.com/ml_use=training",
     validation_filter_split = "labels.aiplatform.googleapis.com/ml_use=validation",
