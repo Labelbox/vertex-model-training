cf37efb45f854a78e8c0580452958439ad70759f
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-07-14 20:58:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def monitor_function(request):
     request_bytes = request.get_data()
     request_json = json.loads(request_bytes)
     time.sleep(300)
-    training_job = aiplatform.AutoMLImageTrainingJob.list(filter=f'display_name={request_json["job_name"]}')[0]
+    training_job = aiplatform.AutoMLImageTrainingJob.list(filter=f'display_name={request_json["model_name"]}')[0]
     job_state = str(training_job.state)
     completed_states = [
         "PipelineState.PIPELINE_STATE_SUCCEEDED",
@@ -36,7 +36,7 @@ def train_function(request):
     request_json = json.loads(request_bytes)
     vertex_dataset = create_vertex_dataset(request_json['model_run_id'], request_json['etl_file'])
     requests.post(request_json['monitor_url'], data=request_bytes)    
-    vertex_model, vertex_model_id = create_autoML_training_job(model_name, vertex_dataset, model_run_id)
+    vertex_model, vertex_model_id = create_autoML_training_job(request_json['model_name'], vertex_dataset, model_run_id)
     print(f'Training Job Name: {model_name}')
     print(f'Vertex Model ID: {vertex_model_id}')
     return "Train Job"
