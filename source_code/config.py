81456d230a13b3823b139d92b678c4f0ad9b5427
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-07-19 14:33:11 -0400

    Update config.py

--- source_code/config.py
+++ source_code/config.py
@@ -1,13 +1,13 @@
 import os
+import time
 from labelbox import Client
 from google.cloud import storage
-import time
 from source_code.errors import MissingEnvironmentVariableException
 
 def get_gcs_client() -> storage.Client:
-    """
+    """ Initiates a Google Cloud client object
     Returns:
-        google cloud storage client.
+        google cloud storage client
     """
     google_project = env_vars("google_project")
     if not google_project:
@@ -15,17 +15,16 @@ def get_gcs_client() -> storage.Client:
     return storage.Client(project=google_project)
 
 def get_lb_client(lb_api_key) -> Client:
-    """
+    """ Initiates a Labelbox client object
     Returns:
-         labelbox client.
+         labelbox client
     """  
     return Client(api_key=lb_api_key, endpoint='https://api.labelbox.com/_gql', enable_experimental=True)
 
 def create_gcs_key(model_run_id: str) -> str:
-    """
-    Utility for creating a gcs key for etl jobs
+    """ Utility for creating a gcs key for the etl job
     Args:
-        job_name: The name of the job (should be named after the etl)
+        job_name    :       The name of the job (should be named after the etl)
     Returns:
         gcs key for the jsonl file.
     """
@@ -33,4 +32,8 @@ def create_gcs_key(model_run_id: str) -> str:
     return f'etl/{model_run_id}/{nowgmt}.jsonl'
 
 def env_vars(value):
+    """ Pulls envionment variables in from Cloud Functions
+    Args:
+        value       :       String representing the environment variable to-be-pulled
+    """
     return os.environ.get(value, '')
